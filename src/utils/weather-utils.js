587f2e422372ef16ec7094b53e544d6f546f5ef6
Braeden <braedenwboyce@gmail.com>
2025-06-12 20:15:19 -0500

    added error checks and made code more consistent

--- src/utils/weather-utils.js
+++ src/utils/weather-utils.js
@@ -88,15 +88,26 @@ const weatherCodes = {
 async function getLocation(location)
 {
     const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=1&language=en&format=json`);
+
+    if (!response.ok)
+    {
+        throw new Error('Could not get location data');
+    }
+
     const data = await response.json();
-    
+
+    if (!data.results || data.results.length === 0)
+    {
+        throw new Error('Location not found');
+    }
+
     const result = data.results[0];
 
     return {
-        name: result.name || "No name availiable",
+        name: result.name || 'No name availiable',
         latitude: result.latitude,
         longitude: result.longitude
-    }
+    };
 }
 
 export async function getWeather(location)
@@ -104,11 +115,17 @@ export async function getWeather(location)
     const { latitude, longitude, name } = await getLocation(location);
 
     const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,is_day,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min`);
+
+    if (!response.ok)
+    {
+        throw new Error('Could not get weather data');
+    }
+
     const data = await response.json();
     
     return {
         name,
         current: data.current,
         daily: data.daily
-    }
+    };
 }
